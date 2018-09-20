f625ea0e0605f70d9509fb1d034bf25252980d4b
Brodie Kelley <brodiekelley@hotmail.com>
2018-09-20 12:31:26 -0600

    Added custom style to Logo component

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 class Logo extends Component {
     render() {
+        const size = {
+            height: this.props.size ? this.props.size : 105,
+            width: this.props.size ? this.props.size : 105
+        }
         return (
             <div className="logo__main">
-                <img alt="DailySmarty UI image logo big" src="/assets/ds_circle_logo.png" />
+                <img style={size} alt="DailySmarty UI image logo big" src="/assets/ds_circle_logo.png" />
             </div>
         );
     }
